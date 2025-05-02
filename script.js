292709394c8b27653c130041889c868416dc2ea4
susnezhu <susan.klimo@gmail.com>
2025-05-02 20:14:14 +0300

    button 1 or 2 dice gameplay

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
+//onko valittu 1 vai 2 noppaa
+let oneDiceChoosed = false;
+let twoDicesChoosed = false;
+
+//ikkunat
 const startMenu = document.getElementById("start_menu");
 const gameWindow = document.getElementById("game_window");
 
+//näppäimet
+const oneDiceBtn = document.getElementById("one_dice");
+const twoDicesBtn = document.getElementById("two_dices");
 const startButton = document.getElementById("start_button");
 
 gameWindow.style.display = "none";
 
+
+//jos klikkaa yhden nopan nappulan
+oneDiceBtn.onclick = function() {
+    if (twoDicesChoosed) {
+        twoDicesChoosed = false
+        twoDicesBtn.style.filter = "";
+    }
+    oneDiceChoosed = true;
+    oneDiceBtn.style.filter = "brightness(85%)";
+}
+
+//jos klikkaa kahden nopan nappulan
+twoDicesBtn.onclick = function() {
+    if (oneDiceChoosed) {
+        oneDiceChoosed = false;
+        oneDiceBtn.style.filter = "";
+    }
+    twoDicesChoosed = true;
+    twoDicesBtn.style.filter = "brightness(85%)";
+}
+
+
 startButton.onclick = function() {
-    gameWindow.style.display = "block";
-    startMenu.style.display = "none"
+    if (oneDiceChoosed | twoDicesChoosed) {
+        gameWindow.style.display = "block";
+        startMenu.style.display = "none";
+    }
+    
 }
